627225382787fce2546cae93835f305f9ad393aa
Steven Liu <59462357+stevhliu@users.noreply.github.com>
2025-06-27 09:38:21 -0700

    [fix] Add FastSpeech2ConformerWithHifiGan (#38207)
    * add to mapping
    
    * oops
    
    * oops
    
    * add to config_mapping_names
    
    * revert
    
    * fix?
    
    * config-mapping-names
    
    * fix?
    
    * fix?

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -129,6 +129,7 @@ CONFIG_MAPPING_NAMES = OrderedDict[str, str](
         ("falcon_h1", "FalconH1Config"),
         ("falcon_mamba", "FalconMambaConfig"),
         ("fastspeech2_conformer", "FastSpeech2ConformerConfig"),
+        ("fastspeech2_conformer_with_hifigan", "FastSpeech2ConformerWithHifiGanConfig"),
         ("flaubert", "FlaubertConfig"),
         ("flava", "FlavaConfig"),
         ("fnet", "FNetConfig"),
@@ -509,6 +510,7 @@ MODEL_NAMES_MAPPING = OrderedDict[str, str](
         ("falcon_h1", "FalconH1"),
         ("falcon_mamba", "FalconMamba"),
         ("fastspeech2_conformer", "FastSpeech2Conformer"),
+        ("fastspeech2_conformer_with_hifigan", "FastSpeech2ConformerWithHifiGan"),
         ("flan-t5", "FLAN-T5"),
         ("flan-ul2", "FLAN-UL2"),
         ("flaubert", "FlauBERT"),
@@ -864,6 +866,7 @@ SPECIAL_MODEL_TYPE_TO_MODULE_NAME = OrderedDict[str, str](
         ("sam_hq_vision_model", "sam_hq"),
         ("llama4_text", "llama4"),
         ("blip_2_qformer", "blip_2"),
+        ("fastspeech2_conformer_with_hifigan", "fastspeech2_conformer"),
     ]
 )
 
@@ -1176,7 +1179,8 @@ class AutoConfig:
 
         >>> unused_kwargs
         {'foo': False}
-        ```"""
+        ```
+        """
         use_auth_token = kwargs.pop("use_auth_token", None)
         if use_auth_token is not None:
             warnings.warn(
