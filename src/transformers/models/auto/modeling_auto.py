caf2fbdebafac695854cf9c648d5153a9665d683
StevenBucaille <steven.bucaille@gmail.com>
2025-07-22 05:53:16 -0400

    Add EfficientLoFTR model (#36355)
    * initial commit
    
    * Apply suggestions from code review
    
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>
    
    * fix: various typos, typehints, refactors from suggestions
    
    * fix: fine_matching method
    
    * Added EfficientLoFTRModel and AutoModelForKeypointMatching class
    
    * fix: got rid of compilation breaking instructions
    
    * docs: added todo for plot
    
    * fix: used correct hub repo
    
    * docs: added comments
    
    * fix: run modular
    
    * doc: added PyTorch badge
    
    * fix: model repo typo in config
    
    * fix: make modular
    
    * fix: removed mask values from outputs
    
    * feat: added plot_keypoint_matching to EfficientLoFTRImageProcessor
    
    * feat: added SuperGlueForKeypointMatching to AutoModelForKeypointMatching list
    
    * fix: reformat
    
    * refactor: renamed aggregation_sizes config parameter into q, kv aggregation kernel size and stride
    
    * doc: added q, kv aggregation kernel size and stride doc to config
    
    * refactor: converted efficientloftr implementation from modular to copied from mechanism
    
    * tests: overwrote batching_equivalence for "keypoints" specific tests
    
    * fix: changed EfficientLoFTRConfig import in test_modeling_rope_utils
    
    * fix: make fix-copies
    
    * fix: make style
    
    * fix: update rope function to make meta tests pass
    
    * fix: rename plot_keypoint_matching to visualize_output for clarity
    
    * refactor: optimize image pair processing by removing redundant target size calculations
    
    * feat: add EfficientLoFTRImageProcessor to image processor mapping
    
    * refactor: removed logger and updated attention forward
    
    * refactor: added auto_docstring and can_return_tuple decorators
    
    * refactor: update type imports
    
    * refactor: update type hints from List/Dict to list/dict for consistency
    
    * refactor: update MODEL_MAPPING_NAMES and __all__ to include LightGlue and AutoModelForKeypointMatching
    
    * fix: change type hint for size parameter in EfficientLoFTRImageProcessor to Optional[dict]
    
    * fix typing
    
    * fix some typing issues
    
    * nit
    
    * a few more typehint fixes
    
    * Remove output_attentions and output_hidden_states from modeling code
    
    * else -> elif to support efficientloftr
    
    * nit
    
    * tests: added EfficientLoFTR image processor tests
    
    * refactor: reorder functions
    
    * chore: update copyright year in EfficientLoFTR test file
    
    * Use default rope
    
    * Add docs
    
    * Update visualization method
    
    * fix doc order
    
    * remove 2d rope test
    
    * Update src/transformers/models/efficientloftr/modeling_efficientloftr.py
    
    * fix docs
    
    * Update src/transformers/models/efficientloftr/image_processing_efficientloftr.py
    
    * update gradient
    
    * refactor: removed unused codepath
    
    * Add motivation to keep postprocessing in modeling code
    
    * refactor: removed unnecessary variable declarations
    
    * docs: use load_image from image_utils
    
    * refactor: moved stage in and out channels computation to configuration
    
    * refactor: set an intermediate_size parameter to be more explicit
    
    * refactor: removed all mentions of attention masks as they are not used
    
    * refactor: moved position_embeddings to be computed once in the model instead of every layer
    
    * refactor: removed unnecessary hidden expansion parameter from config
    
    * refactor: removed completely hidden expansions
    
    * refactor: removed position embeddings slice function
    
    * tests: fixed broken tests because of previous commit
    
    * fix is_grayscale typehint
    
    * not refactoring
    
    * not renaming
    
    * move h/w to embeddings class
    
    * Precompute embeddings in init
    
    * fix: replaced cuda device in convert script to accelerate device
    
    * fix: replaced stevenbucaille repo to zju-community
    
    * Remove accelerator.device from conversion script
    
    * refactor: moved parameter computation in configuration instead of figuring it out when instantiating a Module
    
    * fix: removed unused attributes in configuration
    
    * fix: missing self
    
    * fix: refactoring and tests
    
    * fix: make style
    
    ---------
    
    Co-authored-by: steven <steven.bucaille@buawei.com>
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -113,6 +113,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("dpr", "DPRQuestionEncoder"),
         ("dpt", "DPTModel"),
         ("efficientformer", "EfficientFormerModel"),
+        ("efficientloftr", "EfficientLoFTRModel"),
         ("efficientnet", "EfficientNetModel"),
         ("electra", "ElectraModel"),
         ("emu3", "Emu3Model"),
@@ -321,7 +322,6 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("squeezebert", "SqueezeBertModel"),
         ("stablelm", "StableLmModel"),
         ("starcoder2", "Starcoder2Model"),
-        ("superglue", "SuperGlueForKeypointMatching"),
         ("swiftformer", "SwiftFormerModel"),
         ("swin", "SwinModel"),
         ("swin2sr", "Swin2SRModel"),
@@ -1604,6 +1604,13 @@ MODEL_FOR_KEYPOINT_DETECTION_MAPPING_NAMES = OrderedDict(
     ]
 )
 
+MODEL_FOR_KEYPOINT_MATCHING_MAPPING_NAMES = OrderedDict(
+    [
+        ("efficientloftr", "EfficientLoFTRForKeypointMatching"),
+        ("lightglue", "LightGlueForKeypointMatching"),
+        ("superglue", "SuperGlueForKeypointMatching"),
+    ]
+)
 
 MODEL_FOR_TEXT_ENCODING_MAPPING_NAMES = OrderedDict(
     [
@@ -1759,6 +1766,8 @@ MODEL_FOR_KEYPOINT_DETECTION_MAPPING = _LazyAutoMapping(
     CONFIG_MAPPING_NAMES, MODEL_FOR_KEYPOINT_DETECTION_MAPPING_NAMES
 )
 
+MODEL_FOR_KEYPOINT_MATCHING_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, MODEL_FOR_KEYPOINT_MATCHING_MAPPING_NAMES)
+
 MODEL_FOR_TEXT_ENCODING_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, MODEL_FOR_TEXT_ENCODING_MAPPING_NAMES)
 
 MODEL_FOR_TIME_SERIES_CLASSIFICATION_MAPPING = _LazyAutoMapping(
@@ -1782,6 +1791,10 @@ class AutoModelForMaskGeneration(_BaseAutoModelClass):
 
 class AutoModelForKeypointDetection(_BaseAutoModelClass):
     _model_mapping = MODEL_FOR_KEYPOINT_DETECTION_MAPPING
+
+
+class AutoModelForKeypointMatching(_BaseAutoModelClass):
+    _model_mapping = MODEL_FOR_KEYPOINT_MATCHING_MAPPING
 
 
 class AutoModelForTextEncoding(_BaseAutoModelClass):
@@ -2130,6 +2143,7 @@ __all__ = [
     "MODEL_FOR_IMAGE_SEGMENTATION_MAPPING",
     "MODEL_FOR_IMAGE_TO_IMAGE_MAPPING",
     "MODEL_FOR_KEYPOINT_DETECTION_MAPPING",
+    "MODEL_FOR_KEYPOINT_MATCHING_MAPPING",
     "MODEL_FOR_INSTANCE_SEGMENTATION_MAPPING",
     "MODEL_FOR_MASKED_IMAGE_MODELING_MAPPING",
     "MODEL_FOR_MASKED_LM_MAPPING",
@@ -2174,6 +2188,7 @@ __all__ = [
     "AutoModelForImageToImage",
     "AutoModelForInstanceSegmentation",
     "AutoModelForKeypointDetection",
+    "AutoModelForKeypointMatching",
     "AutoModelForMaskGeneration",
     "AutoModelForTextEncoding",
     "AutoModelForMaskedImageModeling",
