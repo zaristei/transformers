8330ea746c785ab1c40204ac53090eef7e39b25a
lgai-exaone <exaonemodels@lgresearch.ai>
2025-07-26 02:58:28 +0900

    Add EXAONE 4.0 model (#39129)
    * Add EXAONE 4.0 model
    
    * Refactor EXAONE 4.0 modeling code
    
    * Fix cache slicing on SWA + FA2
    
    * Fix cache slicing on FA2 + HybridCache
    
    * Update EXAONE 4.0 modeling code for main branch
    
    * Update o_proj for asymmetric projection
    
    * Address PR feedback
    
    * Add EXAONE 4.0 docs
    
    * Update EXAONE 4.0 modeling code for main branch
    
    * update
    
    * fix updates
    
    * updates
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Arthur <arthur.zucker@gmail.com>
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -112,6 +112,7 @@ if TYPE_CHECKING:
     from .ernie import *
     from .esm import *
     from .evolla import *
+    from .exaone4 import *
     from .falcon import *
     from .falcon_h1 import *
     from .falcon_mamba import *
